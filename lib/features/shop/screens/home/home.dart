ecf74cfeb24518f947daf737305ab284be430a96
raja <rajatraveltsi@gmail.com>
2025-04-11 10:50:37 +0530

    Add existing project files to Git

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -50,7 +50,7 @@ class HomeScreen extends StatelessWidget {
                         SizedBox(height: TSizes.spaceBtwItems),
 
                         /// Categories-body
-                        THomeCategories()
+                        //THomeCategories()
                       ],
                     ),
                   ),
